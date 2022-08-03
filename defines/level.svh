67808df8e0246a8615c453817d0cc5d486d1d618
Evgeny Ostrovsky <evostrov@microsoft.com>
2022-08-03 22:59:12 +0300

    delete level lavel

--- defines/level.svh
+++ defines/level.svh
@@ -1,11 +1,3 @@
 
 parameter logic [10:0] LEVEL_NUMBER_TOP_LEFT_X = 70;
 parameter logic [10:0] LEVEL_NUMBER_TOP_LEFT_Y = 5;
-
-parameter logic [10:0] LEVEL_LABEL_TOP_LEFT_X = 20;
-parameter logic [10:0] LEVEL_LABEL_TOP_LEFT_Y = 5;
-
-parameter logic [10:0] LEVEL_LABEL_WIDTH = 16;
-parameter logic [10:0] LEVEL_LABEL_HEIGHT = 16;
-
-parameter logic [7:0] LEVEL_LABEL_COLOR = 8'hAA;
--- level/level_block.sv
+++ level/level_block.sv
@@ -1,6 +1,3 @@
-import defines::LEVEL_LABEL_TOP_LEFT_X, defines::LEVEL_LABEL_TOP_LEFT_Y;
-import defines::LEVEL_LABEL_WIDTH, defines::LEVEL_LABEL_HEIGHT;
-
 import defines::LEVEL_NUMBER_TOP_LEFT_X, defines::LEVEL_NUMBER_TOP_LEFT_Y;
 import defines::NUMBER_WIDTH, defines::NUMBER_HEIGHT;
 
@@ -14,51 +11,10 @@ module level_block(
 	output	logic	[28:0]	RGBLevel
 );
 
-//************************************************************************************************* Label
-
-logic [10:0] 	offsetXLabel;
-logic [10:0] 	offsetYLabel;
-
-logic 			insideRectangleLabel;
-
-logic			drawLabel;
-logic [28:0]	RGBLabel;
-
-square_object #(.OBJECT_WIDTH(LEVEL_LABEL_WIDTH), .OBJECT_HEIGHT(LEVEL_LABEL_HEIGHT)) square_object_label_inst(
-// input
-	.clk(clk),
-	.resetN(resetN),
-	.pixelX(pixelX),
-	.pixelY(pixelY),
-	.topLeftX(LEVEL_LABEL_TOP_LEFT_X),
-	.topLeftY(LEVEL_LABEL_TOP_LEFT_Y),
-// output
-	.offsetX(offsetXLabel),
-	.offsetY(offsetYLabel),
-	.draw(insideRectangleLabel),
-);
-
-level_label_bitmap level_label_bitmap_inst(
-// input
-	.clk(clk),
-	.resetN(resetN),
-	.offsetX(offsetXLabel),
-	.offsetY(offsetYLabel),
-	.insideRectangle(insideRectangleLabel),
-// output
-	.drawLabel(drawLabel),
-	.RGBLabel(RGBLabel)
-);
-
-//************************************************************************************************* Number
-
 logic [10:0] 	offsetXNumber;
 logic [10:0] 	offsetYNumber;
 
 logic 			insideRectangleNumber;
-
-logic			drawNumber;
-logic [28:0]	RGBNumber;
 
 square_object #(.OBJECT_WIDTH(NUMBER_WIDTH), .OBJECT_HEIGHT(NUMBER_HEIGHT)) square_object_number_inst(
 // input
@@ -83,14 +39,8 @@ number_bitmap number_bitmap_inst(
 	.number(level),
 	.insideRectangle(insideRectangleNumber),
 // output
-	.drawNumber(drawNumber),
-	.RGBNumber(RGBNumber)
+	.drawNumber(drawLevel),
+	.RGBNumber(RGBLevel)
 );
-
-//************************************************************************************************* Output
-
-assign drawLevel = drawLabel || drawNumber;
-
-assign RGBLevel = drawLabel ? RGBLabel : RGBNumber;
 
 endmodule
