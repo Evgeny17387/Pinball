d394240f2f4318ec23766cd31f585a5e3ce8d0ff
Evgeny Ostrovsky <evostrov@microsoft.com>
2022-08-26 17:51:14 +0300

    fix bug in single flipper move

--- rtl/screens/main/flipper/single/flipper_single_controller.sv
+++ rtl/screens/main/flipper/single/flipper_single_controller.sv
@@ -1,27 +1,25 @@
 import defines::FLIPPER_INITIAL_X;
 import defines::FIXED_POINT_MULTIPLIER;
+import defines::SCREEN_MAIN_FLIPPER_SINGLE_X_LIMIT_LEFT, defines::SCREEN_MAIN_FLIPPER_SINGLE_X_LIMIT_RIGHT;
 
 module flipper_single_controller(
-	input	logic					clk,
-	input	logic					resetN,
-	input	logic					startOfFrame,
-	input	logic					key4IsPressed,
-	input	logic					key6IsPressed,
-	input	logic					pause,
-	input	logic					reset_level,
-	input 	logic 					collisionFlipperFrame,
-	input 	logic 			[3:0] 	hitEdgeCode,
-	output	logic signed 	[10:0]	topLeftX,
-	output	logic			[31:0]	speedX
+	input	logic			clk,
+	input	logic			resetN,
+	input	logic			startOfFrame,
+	input	logic			key4IsPressed,
+	input	logic			key6IsPressed,
+	input	logic			pause,
+	input	logic			reset_level,
+	input 	logic			collisionFlipperFrame,
+	input 	logic	[3:0] 	hitEdgeCode,
+	output	logic	[10:0]	topLeftX,
+	output	logic	[31:0]	speedX
 );
 
-const int XspeedCollisionAdd		= 100;
-const int Xspeed					= 500;
+const int XspeedCollisionAdd	= 100;
+const int Xspeed				= 500;
 
 int topLeftX_FixedPoint;
-
-logic collisionOnMoveRight;
-logic collisionOnMoveLeft;
 
 always_ff@(posedge clk or negedge resetN)
 begin
@@ -30,8 +28,6 @@ begin
 
 		topLeftX_FixedPoint <= FLIPPER_INITIAL_X * FIXED_POINT_MULTIPLIER;
 		speedX <= 0;
-		collisionOnMoveRight <= 0;
-		collisionOnMoveLeft <= 0;
 
 	end
 
@@ -41,36 +37,23 @@ begin
 
 			topLeftX_FixedPoint <= FLIPPER_INITIAL_X * FIXED_POINT_MULTIPLIER;
 			speedX <= 0;
-			collisionOnMoveRight <= 0;
-			collisionOnMoveLeft <= 0;
 
 		end
 		else if (!pause) begin
 
 			if (startOfFrame) begin
 
-				if (key6IsPressed && !collisionOnMoveRight) begin
+				if (key6IsPressed && (topLeftX < SCREEN_MAIN_FLIPPER_SINGLE_X_LIMIT_RIGHT)) begin
 					topLeftX_FixedPoint <= topLeftX_FixedPoint + Xspeed;
 					speedX <= XspeedCollisionAdd;
 				end
-				else if (key4IsPressed && !collisionOnMoveLeft) begin
+				else if (key4IsPressed && (topLeftX > SCREEN_MAIN_FLIPPER_SINGLE_X_LIMIT_LEFT + 8)) begin
 					topLeftX_FixedPoint <= topLeftX_FixedPoint - Xspeed;
 					speedX <= -XspeedCollisionAdd;
 				end
 				else begin
 					speedX <= 0;
 				end
-
-				collisionOnMoveRight = 1'b0;
-				collisionOnMoveLeft = 1'b0;
-
-			end
-			else begin
-				
-				if (collisionFlipperFrame && hitEdgeCode[1])
-					collisionOnMoveRight = 1'b1;
-				else if (collisionFlipperFrame && hitEdgeCode[3])
-					collisionOnMoveLeft = 1'b1;
 
 			end
 
